e527dfcdce73ea0f157beac7170b13429fb45b62
waterylime46@aol.com <waterylime46@aol.com>
2019-11-02 11:17:11 -0600

    html done vidoes add broken is the CSS for Port. page and PDF link to res not working

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$('#img1').mouseover( function (){
+console.log('hi')
+$('').play()
+});
+$('#img1').mouseout( function (){
+    console.log('by')
+    $('').pause()
+    });
+
+
+
+    $('.dropdown-menu').toggle
